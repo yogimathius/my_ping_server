ec6ad12c968bf2029b678c5eba4e0d34d08965a2
Mathius <info@yogimathius.dev>
2024-07-29 22:03:26 -0700

    fix: respond with TCP ping instead of http

--- src/server.rs
+++ src/server.rs
@@ -33,9 +33,9 @@ impl Server {
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
-    println!("{}", request_line);
+
     match request_line.as_str() {
-        "GET /ping HTTP/1.1" => respond_with_pong(stream),
+        "ping" => respond_with_pong(stream),
         _ => respond_with_not_found(stream),
     }
 }
@@ -43,12 +43,11 @@ fn handle_connection(mut stream: TcpStream) {
 fn respond_with_pong(mut stream: TcpStream) {
     let utc = Utc::now().to_rfc2822();
     let epoch = DateTime::parse_from_rfc2822(&utc).unwrap().timestamp();
-
-    let response = format!("HTTP/1.1 200 OK\r\n\r\nPong {}", epoch);
+    let response = format!("Pong {}", epoch);
     stream.write_all(response.as_bytes()).unwrap();
 }
 
 fn respond_with_not_found(mut stream: TcpStream) {
-    let response = "HTTP/1.1 404 NOT FOUND\r\n\r\n<!DOCTYPE html><html><head><title>Not Found</title></head><body><h1>Not Found</h1></body></html>";
+    let response = "400 Bad Request\n";
     stream.write_all(response.as_bytes()).unwrap();
 }
