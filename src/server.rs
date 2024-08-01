89c022f9f28bcf4275d20d34544d02361b3a4702
Mathius <info@yogimathius.dev>
2024-07-31 22:37:29 -0700

    refactor: cleanup icmp echo request code

--- src/server.rs
+++ src/server.rs
@@ -73,32 +73,26 @@ async fn send_icmp_echo_requests(hostname: String) {
 
     match ip {
         IpAddr::V4(ipv4) => {
-            println!("Resolved IP: {:?}", ipv4);
             for seq in 0..5 {
-                println!("Sent ICMP echo request to {}", ipv4);
-                let payload = [0; 8];
-                let (_, duration) = surge_ping::ping(ip, &payload).await.unwrap();
-                println!(
-                    "Received ICMP echo reply from {} seq={} time={:?}",
-                    ipv4, seq, duration
-                );
-                sleep(Duration::from_secs(1)).await;
+                send_icmp_echo_request(std::net::IpAddr::V4(ipv4), seq).await;
             }
         }
         IpAddr::V6(ipv6) => {
-            let segments = ipv6.segments();
-            for _ in segments {
-                for seq in 0..5 {
-                    let payload = [0; 8];
-                    println!("Sent TCP echo request to {}", ip);
-
-                    let (_packet, duration) = surge_ping::ping(ip, &payload).await.unwrap();
-                    println!(
-                        "Received TCP echo reply from {} seq={:?} time={:?}",
-                        ipv6, seq, duration
-                    );
-                }
+            for seq in 0..5 {
+                send_icmp_echo_request(std::net::IpAddr::V6(ipv6), seq).await;
             }
         }
     }
+}
+
+async fn send_icmp_echo_request(ip: IpAddr, seq: u16) {
+    let payload = [0; 8];
+    println!("Sent TCP echo request to {}", ip);
+
+    let (_, duration) = surge_ping::ping(ip, &payload).await.unwrap();
+    println!(
+        "Received TCP echo reply from {} seq={:?} time={:?}",
+        ip, seq, duration
+    );
+    sleep(Duration::from_secs(1)).await;
 }
