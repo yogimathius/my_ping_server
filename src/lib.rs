d36abca43e3bb8d9bb0e00e43296aa1ae9e52425
Mathius <info@yogimathius.dev>
2024-09-06 20:56:52 -0700

    refactor: use handlers in server.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,4 @@
+pub mod handlers;
+pub mod request;
+pub mod response;
 pub mod server;
--- src/server.rs
+++ src/server.rs
@@ -1,15 +1,9 @@
-use chrono::{DateTime, Utc};
-use std::net::IpAddr;
-use std::time::Duration;
-use std::{
-    io::{BufRead, BufReader, Write},
-    net::{TcpListener, TcpStream},
-};
-use tokio::time::sleep;
-use trust_dns_resolver::TokioAsyncResolver;
+use std::net::TcpListener;
+
+use crate::handlers::handle_connection;
 pub struct Server {
-    host: String,
-    port: u16,
+    pub host: String,
+    pub port: u16,
 }
 
 impl Server {
@@ -30,68 +24,4 @@ impl Server {
             handle_connection(stream).await;
         }
     }
-}
-
-async fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    if let Some(Ok(request_line)) = buf_reader.lines().next() {
-        if request_line.len() > "ping".len() && request_line.contains("ping") {
-            let host_name = request_line.split_whitespace().last().unwrap().to_string();
-            send_icmp_echo_requests(host_name).await;
-            return;
-        }
-        match request_line.as_str() {
-            "ping" => respond_with_pong(stream),
-            _ => respond_with_not_found(stream),
-        }
-    } else {
-        respond_with_not_found(stream);
-    }
-}
-
-fn respond_with_pong(mut stream: TcpStream) {
-    let utc = Utc::now().to_rfc2822();
-    let epoch = DateTime::parse_from_rfc2822(&utc).unwrap().timestamp();
-    let response = format!("Pong {}", epoch);
-    if let Err(e) = stream.write_all(response.as_bytes()) {
-        eprintln!("Failed to write to stream: {}", e);
-    }
-}
-
-fn respond_with_not_found(mut stream: TcpStream) {
-    let response = "400 Bad Request\n";
-    if let Err(e) = stream.write_all(response.as_bytes()) {
-        eprintln!("Failed to write to stream: {}", e);
-    }
-}
-
-async fn send_icmp_echo_requests(hostname: String) {
-    let resolver = TokioAsyncResolver::tokio_from_system_conf().unwrap();
-    let response = resolver.lookup_ip(hostname).await.unwrap();
-    let ip = response.iter().next().unwrap();
-
-    match ip {
-        IpAddr::V4(ipv4) => {
-            for seq in 0..5 {
-                send_icmp_echo_request(std::net::IpAddr::V4(ipv4), seq).await;
-            }
-        }
-        IpAddr::V6(ipv6) => {
-            for seq in 0..5 {
-                send_icmp_echo_request(std::net::IpAddr::V6(ipv6), seq).await;
-            }
-        }
-    }
-}
-
-async fn send_icmp_echo_request(ip: IpAddr, seq: u16) {
-    let payload = [0; 8];
-    println!("Sent TCP echo request to {}", ip);
-
-    let (_, duration) = surge_ping::ping(ip, &payload).await.unwrap();
-    println!(
-        "Received TCP echo reply from {} seq={:?} time={:?}",
-        ip, seq, duration
-    );
-    sleep(Duration::from_secs(1)).await;
 }
